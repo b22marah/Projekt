e203544222ffaf0d14470e419d40ad0e4fe4b0ea
Marcus Ahlberg <b22marah@student.his.se>
2023-05-21 14:10:09 +0200

    Changed how to program retrieved the data, and added a new class for my "about" screen

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.projekt;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,29 +41,23 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
         RecyclerView view = findViewById(R.id.recyclerview);
         listOfCreatures = new ArrayList<MythicalCreatures>();
         adapter = new MyAdapter(listOfCreatures);
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
-
-        String s = readFile("mountains.json");
-        Gson gson = new Gson();
-        Type type = new TypeToken<ArrayList<MythicalCreatures>>() {}.getType();
-        ArrayList<MythicalCreatures> data = gson.fromJson(s, type);
-        listOfCreatures.addAll(data);
-        adapter.notifyDataSetChanged();
-        Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
-
-        for (int i = 0; i < listOfCreatures.size(); i++) {
-            Log.d("==>", String.valueOf(listOfCreatures.get(i)));
-        }
     }
-
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<MythicalCreatures>>() {}.getType();
+        ArrayList<MythicalCreatures> data = gson.fromJson(json, type);
+        listOfCreatures.addAll(data);
+
+        adapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/projekt/MythicalCreatures.java
+++ app/src/main/java/com/example/projekt/MythicalCreatures.java
@@ -3,24 +3,24 @@ package com.example.projekt;
 public class MythicalCreatures {
     private String ID;
     private String name;
-    private String creator;
-    private String element;
-    private String power;
+    private String type;
+    private String company;
+    private String location;
     private String category;
 
-    public MythicalCreatures(String ID, String name, String creator, String element, String power, String category) {
+    public MythicalCreatures(String ID, String name, String type, String company, String location, String category) {
         this.ID = ID;
         this.name = name;
-        this.creator = creator;
-        this.element = element;
-        this.power = power;
+        this.type = type;
+        this.company = company;
+        this.location = location;
         this.category = category;
 
     }
 
     @Override
     public String toString() {
-        String presentCreatures = "Creatures: " + ID + " Name: " + name + " Element: " + element + " Power: " + power + " Category: " + category + " Creator " + creator;
+        String presentCreatures = "Creatures: " + ID + " Name: " + name + " Element: " + company + " Power: " + location + " Category: " + category + " Creater " + type;
         return presentCreatures;
     }
 }
