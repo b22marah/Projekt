d3c752b2d341b2bed624d00fea58f36e33fb1ee2
Marcus Ahlberg <b22marah@student.his.se>
2023-05-21 13:28:05 +0200

    Created the presentation of MythicalCreatures!!!!!!!

--- app/src/main/java/com/example/projekt/MythicalCreatures.java
+++ app/src/main/java/com/example/projekt/MythicalCreatures.java
@@ -1,4 +1,26 @@
 package com.example.projekt;
 
 public class MythicalCreatures {
+    private String ID;
+    private String name;
+    private String creator;
+    private String element;
+    private String power;
+    private String category;
+
+    public MythicalCreatures(String ID, String name, String creator, String element, String power, String category) {
+        this.ID = ID;
+        this.name = name;
+        this.creator = creator;
+        this.element = element;
+        this.power = power;
+        this.category = category;
+
+    }
+
+    @Override
+    public String toString() {
+        String presentCreatures = "Creatures: " + ID + " Name: " + name + " Element: " + element + " Power: " + power + " Category: " + category + " Creator " + creator;
+        return presentCreatures;
+    }
 }
